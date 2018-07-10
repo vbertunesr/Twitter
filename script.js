021aa6d3c2a8fe73fc0c96b5b97248c50c63f9da
Valeria Bertunes <bertunesrocha@gmail.com>
2018-07-10 16:24:07 -0300

    Versão 0.0.2.2, versão 0.0.3.1.4,versão 0.0.6

--- script.js
+++ script.js
@@ -8,11 +8,22 @@ function tweet(event){
   postado.prepend(minhaDiv);
   minhaDiv.setAttribute('class', 'tweet');
   var newText = document.createElement('p');  
-  newText.textContent = textarea.value;
+  newText.textContent = textarea.value + moment().format('lll');
   minhaDiv.appendChild(newText);
 
   textarea.value = "";
 }
 botao.addEventListener("click",tweet);
 
-
+function desativaBotao(){
+  var limite = 140;
+  var limiteTexto = document.querySelector('#message').value.length;
+  var caracteres = limite - limiteTexto;
+  if(caracteres > 0){ 
+    document.getElementById('limiteTexto').innerHTML = caracteres;
+    document.getElementById('sendButton').disabled = false;
+  }else{
+    document.getElementById('limiteTexto').innerHTML = caracteres;
+    document.getElementById('sendButton').disabled = true;
+  }
+}
